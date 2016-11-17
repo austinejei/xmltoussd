7309d428eff4eec1dba112585c9706d1a5400557
Augustine Adjei <augustine@smsgh.com>
2016-11-17 22:02:57 +0000

    --changed file output settings

--- XmlToUssdCompiler/StringHelper.cs
+++ XmlToUssdCompiler/StringHelper.cs
@@ -10,8 +10,6 @@ namespace XmlToUssdCompiler
             //if doing simulation, it should use the second return, else fine.
 
 
-            if (simulator)
-            {
                 innerText = innerText
                     .Replace("{Request.Mobile}", ConfigurationManager.AppSettings["Ussd.Context.Mobile"])
                     .Replace("{Request.SessionId}", Guid.NewGuid().ToString("N"))
@@ -23,19 +21,7 @@ namespace XmlToUssdCompiler
                     .Replace(@"\t", "\t")
                     //.Replace(@"\r\n", " ")
                     ;
-            }
-
-          
-            innerText = innerText.Replace(Environment.NewLine, string.Empty).Replace(@"\t", "\t");
-            //since we're parsing each expression, we shall have Request object in the expression
-
-            //innerText = innerText.Replace("{Request.Mobile}", "\" + Request.Mobile + \"");
-            //innerText = innerText.Replace("{Request.SessionId}", "\" + Request.SessionId + \"");
-            //innerText = innerText.Replace("{Request.Operator}", "\" + Request.Operator + \"");
-            //innerText = innerText.Replace("{Request.ServiceCode}", "\" + Request.ServiceCode + \"");
-            //innerText = innerText.Replace("{Request.Message}", "\" + Request.Message + \"");
-            //innerText = innerText.Replace("{Request.Type}", "\" + Request.Type + \"");
-            return innerText.Trim();
+        
         }
     }
 }
